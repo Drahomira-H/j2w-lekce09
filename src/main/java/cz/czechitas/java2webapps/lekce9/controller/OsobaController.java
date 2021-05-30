5a96c2f5427bec675aef99173934c6852f78a124
dr4z4 <dr4z4321@gmail.com>
2021-05-30 21:01:16 +0200

    add solution

--- src/main/java/cz/czechitas/java2webapps/lekce9/controller/OsobaController.java
+++ src/main/java/cz/czechitas/java2webapps/lekce9/controller/OsobaController.java
@@ -1,31 +1,78 @@
 package cz.czechitas.java2webapps.lekce9.controller;
-
 import cz.czechitas.java2webapps.lekce9.service.OsobaService;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
-
+import org.springframework.web.util.UriComponentsBuilder;
+import org.springframework.web.util.UrlPathHelper;
+import javax.servlet.http.HttpServletRequest;
 /**
  * Controller pro zobrazování seznamů osob.
  */
 @Controller
 public class OsobaController {
   private final OsobaService service;
-
   public OsobaController(OsobaService service) {
     this.service = service;
   }
-
   @GetMapping("/")
   public ModelAndView zakladniSeznam(@PageableDefault(sort = {"prijmeni", "jmeno"}) Pageable pageable) {
     return new ModelAndView("osoby")
             .addObject("osoby", service.seznamOsob(pageable));
   }
 
+  @GetMapping("/dle-data-narozeni")
+  public ModelAndView dleDataNarozeni(@PageableDefault(sort = {"datumNarozeni", "prijmeni"}) Pageable pageable) {
+    return new ModelAndView("osoby")
+            .addObject("osoby", service.seznamOsob(pageable));
+  }
+
+  @GetMapping("/rok-narozeni")
+  public ModelAndView rokNarozeni(@RequestParam("od") int rokOd, @RequestParam("do") int rokDo, @PageableDefault(sort = {"datumNarozeni", "prijmeni"}) Pageable pageable) {
+    return new ModelAndView("osoby")
+            .addObject("osoby", service.seznamDleRokuNarozeni(rokOd, rokDo, pageable));
+  }
+
+  @GetMapping("/prijmeni")
+  public ModelAndView prijmeni(String prijmeni, @PageableDefault() Pageable pageable) {
+    return new ModelAndView("osoby")
+            .addObject("osoby", service.seznamDlePrijmeni(prijmeni, pageable));
+  }
+
+  @GetMapping("/obec")
+  public ModelAndView obec(String obec, @PageableDefault(sort = {"prijmeni", "jmeno"}) Pageable pageable) {
+    return new ModelAndView("osoby-s-adresou")
+            .addObject("osoby", service.seznamDleObce(obec, pageable));
+  }
+
+  @GetMapping("/minimalni-vek")
+  public ModelAndView minimalniVek(int vek, @PageableDefault(sort = {"prijmeni", "jmeno"}) Pageable pageable) {
+    return new ModelAndView("osoby")
+            .addObject("osoby", service.seznamDleVeku(vek, pageable));
+  }
+
   @GetMapping("/vyber")
   public String vyber() {
     return "vyber";
+  }
+  /**
+   * Získání aktuální URL s query parametry bez parametrů {@code size} a {@code page}.
+   *
+   */
+  @ModelAttribute("currentURL")
+  public String currentURL(HttpServletRequest request) {
+    UrlPathHelper helper = new UrlPathHelper();
+    return UriComponentsBuilder
+            .newInstance()
+            .path(helper.getOriginatingRequestUri(request))
+            .query(helper.getOriginatingQueryString(request))
+            .replaceQueryParam("size")
+            .replaceQueryParam("page")
+            .build()
+            .toString();
   }
 }
--- src/main/java/cz/czechitas/java2webapps/lekce9/service/OsobaService.java
+++ src/main/java/cz/czechitas/java2webapps/lekce9/service/OsobaService.java
@@ -1,5 +1,4 @@
 package cz.czechitas.java2webapps.lekce9.service;
-
 import cz.czechitas.java2webapps.lekce9.entity.Osoba;
 import cz.czechitas.java2webapps.lekce9.repository.OsobaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,23 +6,53 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+
 /**
  * Služba pro práci s osobami a adresami.
  */
 @Service
 public class OsobaService {
   private final OsobaRepository osobaRepository;
-
   @Autowired
   public OsobaService(OsobaRepository osobaRepository) {
     this.osobaRepository = osobaRepository;
   }
 
   /**
-   * Vrací stránkovaný seznam všech osob v databázi seřazených podle příjmení a jména.
+   * Vrací stránkovaný seznam všech osob v databázi.
    */
   public Page<Osoba> seznamOsob(Pageable pageable) {
     return osobaRepository.findAll(pageable);
+  }
+
+  /**
+   * Vrací stránkovaný seznam všech osob v databázi, které se narodili mezi uvedenými roky.
+   */
+  public Page<Osoba> seznamDleRokuNarozeni(int rokOd, int rokDo, Pageable pageable) {
+    return osobaRepository.findByRok(rokOd, rokDo, pageable);
+  }
+
+  /**
+   * Vrací stránkovaný seznam všech osob v databázi, jejichž příjmení začíná uvedeným textem.
+   */
+  public Page<Osoba> seznamDlePrijmeni(String prijmeni, Pageable pageable) {
+    return osobaRepository.findByPrijmeniStartingWithIgnoreCase(prijmeni, pageable);
+  }
+
+  /**
+   * Vrací stránkovaný seznam všech osob v databázi, které bydlí v uvedené obci.
+   */
+  public Page<Osoba> seznamDleObce(String obec, Pageable pageable) {
+    return osobaRepository.findByObec(obec, pageable);
+  }
+
+  /**
+   * Vrací stránkovaný seznam všech osob v databázi, které dosáhli zadaného věku.
+   */
+  public Page<Osoba> seznamDleVeku(int vek, Pageable pageable) {
+    LocalDate date = LocalDate.now().minusYears(vek);
+    return osobaRepository.findByDatumNarozeniBefore(date, pageable);
   }
 
 }
